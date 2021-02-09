c789ee5d8e7a80b4a9a302f37c1ee71ddf39e4ff
hobbyluv <princewillchime43@gmail.com>
2021-02-09 19:12:56 +0100

    created the model for  client and admin registration

---
+++ model/AdminSignUp.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AdminData = new Schema(
+  {
+    email: { type: String, required: true, trim: true },
+    password: { type: String, trim: true, required: true },
+    AdminId: { type: String, required: true },
+  },
+
+  { timestamps: true }
+);
+
+const AdminSignUp = mongoose.model("adminData", AdminData);
+
+module.exports = { AdminSignUp };
---
+++ model/ClientSignUp.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const contact = {
+  state: {
+    type: String,
+    default: "",
+    trim: true,
+  },
+  city: {
+    type: String,
+    default: "",
+    trim: true,
+  },
+
+  address: {
+    type: String,
+    default: "",
+    trim: true,
+  },
+};
+
+const ClientData = new Schema(
+  {
+    firstName: { type: String, trim: true, required: true },
+    lastName: { type: String, trim: true, required: true },
+    email: { type: String, required: true, trim: true },
+    profileImage: { type: String, trim: true, default: "" },
+    password: { type: String, trim: true, required: true },
+    phone: { type: Number },
+    contact: contact,
+    posts: { type: Number, default: 0 },
+    registerdIpAddress: { type: String, default: "" },
+  },
+
+  { timestamps: true }
+);
+
+const ClientStatus = new Schema(
+  {
+    loginedIn: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+ClientData.add(ClientStatus);
+
+const ClientSignUp = mongoose.model("ClientData", ClientData);
+
+module.exports = { ClientSignUp };
